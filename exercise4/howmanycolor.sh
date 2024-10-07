ea96c67caeeb43f04f898a299d87d7638ebf94f9
Cupid-0x80h <ccuppid09@gmail.com>
2024-10-07 23:51:59 +0545

    Add files via upload

---
+++ exercise4/howmanycolor.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rainbow=("violet" "indigo" "blue" "green" "yellow" "orange" "red")
+echo "Colors of the rainbow:"
+echo ${rainbow[*]}
+
+echo "Numbers of colors in rainbows :"
+echo ${#rainbow[@]}
